79022a4314317ec40e8f7595bdaa74cb93c84177
KiselevaOlga <jjcafe09@gmail.com>
2021-01-30 17:11:31 +0000

    fi

--- social_blog/settings.py
+++ social_blog/settings.py
@@ -144,4 +144,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'olga.kiseleva.pro@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_PASSWORD = 'inwltbhetqqtwxra'
